349272588f25b2ae2ea50fe573ae62ee0606ec0f
KallDrexx <me@mshapiro.net>
2024-04-17 21:58:55 -0400

    Initial commit

---
+++ RemoteLea.Core/IInstruction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace RemoteLea.Core;
+
+/// <summary>
+/// Defines a parameter that can be given to an instruction
+/// </summary>
+public record InstructionParameter(string Name, string Description);
+
+/// <summary>
+/// Defines a value that is created by an instruction and can be placed in a variable for
+/// later reference.
+/// </summary>
+public record InstructionOutput(string Name, string Description);
+
+/// <summary>
+/// Defines a specific instruction that's been implemented
+/// </summary>
+/// <param name="OpCode">String that uniquely identifies a single instruction</param>
+/// <param name="Parameters">Named parameters for each instruction</param>
+/// <param name="Outputs">Named outputs that are saved</param>
+public record InstructionDefinition(string OpCode, IReadOnlyList<string> Parameters, IReadOnlyList<string> Outputs);
+
+/// <summary>
+/// An instruction contains execution logic for performing a type of operation. Each implementation
+/// is intended to be stateless and called each time the instruction's opcode is encountered.
+/// </summary>
+public interface IInstruction
+{
+    /// <summary>
+    /// Retrieves the definition for this instruction
+    /// </summary>
+    InstructionDefinition Definition { get; }
+}
---
+++ RemoteLea.Core/InstructionSet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace RemoteLea.Core;
+
+/// <summary>
+/// Description for how to run a specific instruction
+/// </summary>
+/// <param name="OpCode">The op code for the desired instruction to run</param>
+/// <param name="Arguments">Named arguments being executed</param>
+/// <param name="OutputVariableNames">Names of the variables to save each output to</param>
+public record ExecutableInstruction(
+    string OpCode,
+    IReadOnlyDictionary<string, string> Arguments,
+    IReadOnlyDictionary<string, string> OutputVariableNames);
+
+/// <summary>
+/// A set of instructions that can be executed in order
+/// </summary>
+public class InstructionSet : IEnumerator<ExecutableInstruction>
+{
+    public bool MoveNext()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Reset()
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    public void Dispose()
+    {
+        // TODO release managed resources here
+    }
+
+    public ExecutableInstruction Current { get; }
+
+    object IEnumerator.Current => Current;
+}
